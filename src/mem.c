78641103416312f62ebe890ed15b97bc19a101af
ccheerry <alvarocherry0@gmail.com>
2025-10-05 17:16:51 +0200

    fix(leaks): some, still some leaks exist and segfaults

--- src/mem.c
+++ src/mem.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   mem.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: albcamac <albcamac@student.42.fr>          +#+  +:+       +#+        */
+/*   By: acerezo- <acerezo-@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/15 17:13:18 by acerezo-          #+#    #+#             */
-/*   Updated: 2025/09/26 15:21:03 by albcamac         ###   ########.fr       */
+/*   Updated: 2025/10/05 17:15:38 by acerezo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,9 @@
 
 void	free_mlx(t_game *game)
 {
+	int	i;
+
+	i = -1;
 	if (game->mlx)
 	{
 		if (game->frame.img)
@@ -26,6 +29,12 @@ void	free_mlx(t_game *game)
 			mlx_destroy_image(game->mlx, game->map.textures.east.img);
 		if (game->map.textures.west.img)
 			mlx_destroy_image(game->mlx, game->map.textures.west.img);
+		// if (&game->door_tex)
+		mlx_destroy_image(game->mlx, game->door_tex.img);
+		free(game->doors);
+		free(game->enemies);
+		while (++i < ENEMY_FRAMES)
+			mlx_destroy_image(game->mlx, game->enemy_anim.frames[i].img);
 		if (game->win)
 			mlx_destroy_window(game->mlx, game->win);
 		(mlx_destroy_display(game->mlx), free(game->mlx));
