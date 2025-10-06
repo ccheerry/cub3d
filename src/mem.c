303c66072d0efc0fc5bb9eeb7f87878b9da2c812
Cherry <alvarocherry0@gmail.com>
2025-10-06 16:10:55 +0200

    fix(leaks): bonus and fixed segfaults

--- src/mem.c
+++ src/mem.c
@@ -6,17 +6,30 @@
 /*   By: acerezo- <acerezo-@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/15 17:13:18 by acerezo-          #+#    #+#             */
-/*   Updated: 2025/10/05 17:15:38 by acerezo-         ###   ########.fr       */
+/*   Updated: 2025/10/06 15:59:23 by acerezo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-void	free_mlx(t_game *game)
+static void	free_bonus(t_game *game)
 {
 	int	i;
 
 	i = -1;
+	if (game->door_tex.img)
+		mlx_destroy_image(game->mlx, game->door_tex.img);
+	ft_free((void **)&game->doors);
+	ft_free((void **)&game->enemies);
+	while (++i < ENEMY_FRAMES)
+	{
+		if (game->enemy_anim.frames[i].img)
+			mlx_destroy_image(game->mlx, game->enemy_anim.frames[i].img);
+	}
+}
+
+void	free_mlx(t_game *game)
+{
 	if (game->mlx)
 	{
 		if (game->frame.img)
@@ -29,12 +42,7 @@ void	free_mlx(t_game *game)
 			mlx_destroy_image(game->mlx, game->map.textures.east.img);
 		if (game->map.textures.west.img)
 			mlx_destroy_image(game->mlx, game->map.textures.west.img);
-		// if (&game->door_tex)
-		mlx_destroy_image(game->mlx, game->door_tex.img);
-		free(game->doors);
-		free(game->enemies);
-		while (++i < ENEMY_FRAMES)
-			mlx_destroy_image(game->mlx, game->enemy_anim.frames[i].img);
+		free_bonus(game);
 		if (game->win)
 			mlx_destroy_window(game->mlx, game->win);
 		(mlx_destroy_display(game->mlx), free(game->mlx));
