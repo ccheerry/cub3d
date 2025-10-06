68f0afdffc7d5a6b5f2fa4c982b7cb34ec715f03
ccheerry <alvarocherry0@gmail.com>
2025-10-06 21:35:53 +0200

    feat(mouse): proper cluster sens

--- include/cub3d.h
+++ include/cub3d.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   cub3d.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: albcamac <albcamac@student.42.fr>          +#+  +:+       +#+        */
+/*   By: acerezo- <acerezo-@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/02 19:10:02 by acerezo-          #+#    #+#             */
-/*   Updated: 2025/10/06 21:10:34 by albcamac         ###   ########.fr       */
+/*   Updated: 2025/10/06 21:33:06 by acerezo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -113,7 +113,7 @@ double	sqrt(double x);
 
 /* === Mouse look === */
 # ifndef MOUSE_SENS
-#  define MOUSE_SENS 0.05f/* grados por píxel */
+#  define MOUSE_SENS 0.11f/* grados por píxel */
 # endif
 # ifndef MOUSE_MAX_STEP
 #  define MOUSE_MAX_STEP 20/* píxeles máx por evento */
